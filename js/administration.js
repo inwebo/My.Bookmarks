92a458e273588d9290e6ab5a10d1a92beaf24371
Inwebo <inwebo@gmail.com>
2011-12-02 23:55:01 +0100

    Update flow #2

---
+++ js/administration.js
@@ -0,0 +1,36 @@
+/**
+ * Administration
+ */
+(function($) {
+    $(function() {
+        
+        // Delete categorie
+        $('#categoriesList p.ui-droppable span').click(function() {
+            var i = $(this);
+            var r = confirm('Delete categorie number : ' + $(this).attr('data-category') + ' ( id='+$(this).attr('data-id')+' ) ?'+"\n");
+            if(r==true) {
+                $.ajax({
+                    type: "POST",
+                    // URL script PHP
+                    url: hostRoot+"del-categorie.php",
+                    data: {
+                        delCat: $(this).attr('data-id')
+                    },
+                    dataType: "text",
+                    beforeSend:function() {
+                    },
+                    success:function(data) {
+                        var categorieName = i.parent('.delete').attr('data-category');
+                        i.parent('p').slideUp(300);
+                        
+                        addMssg( 'okay', 'Categorie ' + categorieName + ' deleted.' );
+                    },
+                    error:function() {
+                        addMssg( 'error', 'Cannot delete element.' );
+                    }
+                })
+            }
+        });
+        
+    });
+})(jQuery);
