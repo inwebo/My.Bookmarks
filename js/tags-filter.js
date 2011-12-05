47d4f54195e713fb2a31f28b69934f27e38e36c9
Inwebo <inwebo@gmail.com>
2011-12-05 20:17:07 +0100

    Edit readme

--- js/tags-filter.js
+++ js/tags-filter.js
@@ -5,7 +5,7 @@ $('#tagsFilter').keyup(function() {
     if($('#tagsFilter').val() == '' ) {
         $('.tags').fadeIn('fast');
     }
-else {
+    else {
         $.each( $('.tags:not([data-tags^="'+$('#tagsFilter').val()+'"])'), function(index, value){
             console.log(this);
             $(this).fadeOut('fast');
