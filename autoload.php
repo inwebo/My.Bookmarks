43c261a93403dcf8d56bade29ae4995fdf0f640b
inwebo <gh_kvrg1hir>
2012-01-18 23:33:03 +0100

    test

--- autoload.php
+++ autoload.php
@@ -39,7 +39,7 @@
  */
 ?>
 <?php
-function __autoload( $class_name ) {
+function __autoload( $class_name ) { 
     include_once 'lib/' . strtolower( $class_name ) . '/class.' . strtolower($class_name) . '.php';
 }
 ?>
