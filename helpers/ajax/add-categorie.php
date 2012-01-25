34d147b50a315012a062a47f5cc8359423e23790
inwebo <gh_kvrg1hir>
2012-01-25 19:45:16 +0100

    Pdo

--- helpers/ajax/add-categorie.php
+++ helpers/ajax/add-categorie.php
@@ -43,13 +43,14 @@ chdir('..');
 chdir('..');
 include('autoload.php');
 include( 'helpers/const.define.php');
-$newCat = $sql->query( 'INSERT INTO `categories` (`name`) VALUES (":?")', array( $_POST['inputCat'] ) ) ;
-if( $newCat === FALSE ) {
+$newCat = $sql->query( 'INSERT INTO `categories` (`name`) VALUES ("?")', array( $_POST['inputCat'] ) ) ;
+if( count($newCat) == 0 ) {
 	echo 'FALSE';
 }
 else {
-	$newIdCat = $sql->query( 'SELECT `id` FROM `categories` WHERE `name`=":?"', array( $_POST['inputCat'] ) ) ;
-	while ($row = mysql_fetch_assoc( $newIdCat )) {
+	$newIdCat = $sql->query( 'SELECT `id` FROM `categories` WHERE `name`="?"', array( $_POST['inputCat'] ) ) ;
+	//while ($row = mysql_fetch_assoc( $newIdCat )) {
+	foreach( $newIdCat as $row ) {
 		echo $row['id']."\n";
 	}
 }
