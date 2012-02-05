3f00b07696c59d335318f44d2429f3bbbd2473c3
Inwebo <inwebo@gmail.com>
2012-02-05 02:46:03 +0100

    Renamed in more logical way

--- helpers/backup.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-// @todo 
-// SELECT * INTO OUTFILE 'backup.sql' FROM bookmarks, categories,role,trash,users
-?>
