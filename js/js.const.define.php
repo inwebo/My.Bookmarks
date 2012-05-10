ddc45b2b7be7c10357e944cd22bf07eac979bf73
Inwebo <inwebo@gmail.com>
2012-05-10 12:59:32 +0200

    All PHP consts are available in jquery scope

--- js/js.const.define.php
+++ js/js.const.define.php
@@ -3,11 +3,19 @@ header('Content-type: application/javascript');
 chdir('../');
 include('autoload.php');
 include('helpers/const.define.php');
+$constantes = get_defined_constants( true );
+$constantes = $constantes['user'];
+foreach($constantes as $key=>$value) {
+    $prefix = 'JS_';
+    echo $prefix . $key . '="' . $value  . '";' ."\n";
+}
 
-$output  = 'JS_PATH_AJAX="'  . PATH_AJAX   . '";' ."\n";
-$output .= 'JS_PATH_INDEX="' . PATH_INDEX  . '";' ."\n";
-$output .= 'JS_PATH_JS="'    . PATH_JS     . '";' ."\n";
-$output .= 'JS_PUBLIC_KEY="' . PUBLIC_KEY  . '";' ."\n";
-$output .= 'JS_DEBUG='       . APP_DEBUG       . ';'  ."\n";
 
-echo $output;
+$ajax_files = new MyDirectory('js/ajax/');
+
+foreach($ajax_files->ret['Files'] as $key => $value ) {
+    $toConst = str_replace('-', '_', $value->nameLessExtension);
+    $toConst = strtoupper($toConst);
+
+    echo 'JS_PATH_AJAX_'.$toConst. '="'.PATH_AJAX.$value->name.'";'. "\n";
+}
