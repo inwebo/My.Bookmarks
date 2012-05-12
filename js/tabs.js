1749c8725cd5485efee8b8a1f85818699ea6d3dd
Inwebo <inwebo@gmail.com>
2012-05-12 13:08:20 +0200

    tabs plugin

---
+++ js/tabs.js
@@ -0,0 +1,14 @@
+(function($){
+$('#tabs div').hide();
+$('#tabs div:first').show();
+$('#tabs ul li:first').addClass('active');
+
+$('#tabs ul li a').click(function(){
+$('#tabs ul li').removeClass('active');
+$(this).parent().addClass('active');
+var currentTab = $(this).attr('href');
+$('#tabs div').hide();
+$(currentTab).show();
+return false;
+});
+})(jQuery)
