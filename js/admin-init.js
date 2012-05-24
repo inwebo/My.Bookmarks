6791b8614524f33a35303b91b41769cf24d7f0ce
inwebo <inwebo@gmail.com>
2012-05-25 00:35:06 +0200

    Fixed selector

--- js/admin-init.js
+++ js/admin-init.js
@@ -16,16 +16,16 @@
 	 */
 	$( '.categorie-menu-display-full' ).click( function(){
 		// #1 : Est une vue compacte ?
-		if( $( '.bookmarks-list' ).hasClass( 'bookmarks-simple' ) ) {
-			$( '.bookmarks-list' ).removeClass( 'bookmarks-simple' );
+		if( $(this).closest('div[class*=grid_]').children( '.bookmarks-list' ).hasClass( 'bookmarks-simple' ) ) {
+			$(this).closest('div[class*=grid_]').children( '.bookmarks-list' ).removeClass( 'bookmarks-simple' );
 		}
 		return false;
 	});
 
 	$( '.categorie-menu-display-compact' ).click( function(){
 		// #1 : N'est pas une vue compacte ?
-		if( !$( '.bookmarks-list' ).hasClass( 'bookmarks-simple' ) ) {
-			$( '.bookmarks-list' ).addClass( 'bookmarks-simple' );
+		if( !$(this).closest('div[class*=grid_]').children( '.bookmarks-list' ).hasClass( 'bookmarks-simple' ) ) {
+			$(this).closest('div[class*=grid_]').children( '.bookmarks-list' ).addClass( 'bookmarks-simple' );
 		}
 		return false;
 	});
