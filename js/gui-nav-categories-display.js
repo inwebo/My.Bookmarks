6c66d2dd0026baba1532bc55aa7081e18bc0dde4
inwebo <inwebo@gmail.com>
2012-05-28 18:23:09 +0200

    Escape any string before loading

--- js/gui-nav-categories-display.js
+++ js/gui-nav-categories-display.js
@@ -1,6 +1,11 @@
-(function($){
+;(function($) {
+
 var droppableContainer = '#categories-landing';
+var dropdown           = 0;
+
 $('#menu-display-categories').click(function(){
+	
+	if( dropdown == 0 ) {
 		$(droppableContainer).animate({
 			'top' : 30
 		}, 800, 'easeOutQuint');
@@ -8,6 +13,23 @@ $('#menu-display-categories').click(function(){
 		$('h1 span').animate({
 			'margin-top' : -10
 		}, 800, 'easeOutQuint');
+		
+		$(this).removeClass('');
+		
+		dropdown = 1;
+	}
+	else {
+		$(droppableContainer).animate({
+			'top' : -100
+		}, 800, 'easeOutQuint');
+		
+		$('h1 span').animate({
+			'margin-top' : 0
+		}, 800, 'easeOutQuint');
+		
+		dropdown = 0;
+	}
+
 });
 
 })(jQuery)
