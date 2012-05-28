d761333d4bb72f2f9089e6297ed852f09b4824d6
Inwebo <inwebo@gmail.com>
2012-05-28 23:48:00 +0200

    JS const

---
+++ helpers/js.const.define.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-type: application/javascript');
+chdir('../');
+include('autoload.php');
+include('helpers/const.define.php');
+$constantes = get_defined_constants( true );
+$constantes = $constantes['user'];
+
+foreach( $constantes as $key => $value ) {
+    $prefix = 'JS_';
+    echo $prefix . $key . '="' . $value  . '";' ."\n";
+}
+
+$ajax_files = new MyDirectory( 'js/ajax' );
+
+foreach($ajax_files->ret['Files'] as $key => $value ) {
+    $toConst = str_replace('-', '_', $value->nameLessExtension);
+    $toConst = strtoupper($toConst);
+	echo $toConst . "\n";
+    //echo 'JS_PATH_AJAX_'.$toConst. '="JS_PATH_AJAX_'.$value->name.'";'. "\n";
+}
