1d1f9ce25b4d92d79d75029b297f92b7a3dc2f36
Inwebo <inwebo@gmail.com>
2012-05-28 23:48:42 +0200

    JS files includer

---
+++ helpers/js.include-gui.php
@@ -0,0 +1,8 @@
+<?php
+	extract( $GLOBALS );
+?>
+<script type="text/javascript" src="<?php echo PATH_JS_CONST; ?>"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>gui-login.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>gui-nav-categories-display.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>gui-bookmarks-list-switch-view.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.mynotifications.js"></script>
