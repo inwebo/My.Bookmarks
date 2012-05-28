50243a2742227dffea8993662acbae680ae1264a
Inwebo <inwebo@gmail.com>
2012-05-28 23:47:27 +0200

    Init public plugins

--- js/init-gui.js
+++ js/init-gui.js
@@ -1,16 +1,4 @@
 ;(function($){
-	
-	pluginNotifications;
-	
-	$.getScript( JS_PATH_JS + "gui-login.js");
-	$.getScript( JS_PATH_JS + "gui-nav-categories-display.js");
-	$.getScript( JS_PATH_JS + "gui-bookmarks-list-switch-view.js");
-	$.getScript( JS_PATH_JS + "plugin.mynotifications.js", function(xhr){
-		eval(xhr);
-	    pluginNotifications = new $.myNotifications();	
-	});
-	$.getScript( JS_PATH_JS + "plugin.tags-filter.js", function(){
-		pluginTagsFilter    = new $.myTagsFilter();
-	});
-
+    pluginNotifications = new $.myNotifications();	
+	pluginTagsFilter    = new $.myTagsFilter();
 })(jQuery);
