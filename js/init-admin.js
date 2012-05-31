bb49d6cad0828745f0bb504fab8f84925da8c358
inwebo <inwebo@gmail.com>
2012-05-31 20:31:42 +0200

    New handler

--- js/init-admin.js
+++ js/init-admin.js
@@ -1,5 +1,14 @@
 $(window).load(function(){
 	console.log('init admin');
-	window.pluginBookmarksHandler = new $.myBookmarksHandler();
+	window.pluginBookmarksHandler  = new $.myBookmarksHandler();
+	window.pluginCategoriesHandler = new $.myCategoriesHandler();
+	
+	/* Handler modification categories */
+	var listSortable = "#sortable";
+	$( listSortable ).sortable({
+		placeholder: "categories-sort-placeholder",
+		stop:function(){ window.pluginCategoriesHandler.Query() }
+	});
+	$( listSortable ).disableSelection();
 	
 });
