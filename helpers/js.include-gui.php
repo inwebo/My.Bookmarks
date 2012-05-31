9b6dc234b180e12f9274f2c88e00c0328f41e1c2
inwebo <inwebo@gmail.com>
2012-05-31 04:08:04 +0200

    Plugins added

--- helpers/js.include-gui.php
+++ helpers/js.include-gui.php
@@ -6,3 +6,4 @@
 <script type="text/javascript" src="<?php echo PATH_JS; ?>gui-nav-categories-display.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>gui-bookmarks-list-switch-view.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.mynotifications.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.tags-filter.js"></script>
