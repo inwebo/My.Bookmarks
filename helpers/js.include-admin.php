ff817511ebbacea6f9f6c961ce78983ba37f2e92
inwebo <inwebo@gmail.com>
2012-05-31 20:30:11 +0200

    Plugin added

--- helpers/js.include-admin.php
+++ helpers/js.include-admin.php
@@ -2,7 +2,7 @@
 <script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.bookmarks.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>admin-bookmarks.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>admin-categories.js"></script>
-<script type="text/javascript" src="<?php echo PATH_JS; ?>admin-categories-sort.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>admin-tabs.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>tabs.js"></script>
 <script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.gui-bookmarks.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.gui-categories.js"></script>
