36b60b51c6838b5e7957bf3db03ec20cc6e1f199
inwebo <inwebo@gmail.com>
2012-05-31 04:12:43 +0200

    JS admin includer

---
+++ helpers/js.include-admin.php
@@ -0,0 +1,8 @@
+<?php extract($GLOBALS); ?>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.bookmarks.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>admin-bookmarks.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>admin-categories.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>admin-categories-sort.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>admin-tabs.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>tabs.js"></script>
+<script type="text/javascript" src="<?php echo PATH_JS; ?>plugin.gui-bookmarks.js"></script>
