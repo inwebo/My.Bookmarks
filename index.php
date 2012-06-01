5a3e3a24c215364b41e930561415176d1cc844f4
inwebo <inwebo@gmail.com>
2012-06-01 16:50:45 +0200

    Translate

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 	}
 ?>
 <?php 
-
+// Translate
 $lang = 'fr_FR.utf8';
 $filename = 'message';
-
 putenv("LC_ALL=$lang");
 setlocale(LC_ALL, $locale);
 bindtextdomain($filename,"./locale");
 bind_textdomain_codeset($filename, "UTF-8");
 textdomain("messages"); 
 ?>
+
 <?php include('autoload.php'); ?>
 <?php include( dirname( __FILE__ ) . DIRECTORY_SEPARATOR .'helpers/const.define.php' ); ?>
 
@@ -105,7 +105,6 @@ if( isset( $_GET['q'] ) && $_SESSION['type'] == 'admin' ) {
 </head>
 
 <body>
-	<?php echo _('Hello'); ?>
     <div class="gui-display-shaddy">&nbsp;</div>
 	<?php //if( $_SESSION['type'] == 'admin' ) { ?>
     <div id="categories-landing">
@@ -191,9 +190,6 @@ if( isset( $_GET['q'] ) && $_SESSION['type'] == 'admin' ) {
 			}
 		?>
  		<!-- Include -->
-                
-	
-
         <div class="clear"></div>
 
 	</div>
