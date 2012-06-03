6801461db870820a9ba5c448b9a63f091e923b93
inwebo <inwebo@gmail.com>
2012-06-03 22:01:13 +0200

    Clean

--- js/admin-tabs.js
+++ js/admin-tabs.js
@@ -4,6 +4,8 @@
 (function($) {
     $(function() {
         var $items = $('#vtab>ul>li');
+        $('.tabs-content div').hide();
+        $('.tabs-content div').eq(0).show();
         $items.click(function() {
             $items.removeClass('selected');
             $(this).addClass('selected');
--- js/tabs.js
+++ js/tabs.js
@@ -11,4 +11,7 @@
 		$(currentTab).show();
 		return false;
 	});
+	
+
+	
 })(jQuery)
